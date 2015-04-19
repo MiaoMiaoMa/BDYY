9d6fee90d6841e23b95fb2692c0f7243fb63c5fc
MiaoMiaoMa <newmcz@yahoo.com>
2015-04-19 20:23:47 +0800

    Updated some code 2015-4-19 20:23

--- BDYY_WEB/Scripts/Admin/drugstoreManage.js
+++ BDYY_WEB/Scripts/Admin/drugstoreManage.js
@@ -26,4 +26,46 @@
 
     //初始化数据
     $scope.onSearch();
+
+
+    $scope.showApprove = function ($index) {
+        //alert($index);
+        var bh = $(document).height();
+        var bw = $(window).width();
+        var wh = $(window).height();
+        var popwidth = 900;
+        var popheight = 550;
+        var v_left = (bw - popwidth) / 2;
+        var v_top = bh > popheight ? (bh - popheight) / 2 : 0;
+        $('.edialogheadertitle').text($scope.titleBig);
+        $(".bg").css({ width: bw, height: bh, display: "block" })
+        $('.pop_up_box').css({ width: popwidth, height: popheight });
+        $('.pop_up_box').css({ left: v_left, top: v_top, display: "block" });
+        $("#ApproveDiv").show();
+
+        $window.callAJAX('../Account/GetPatientAllInfo', { uid: $index }, 'JSON', function (data) {
+            if (data) {
+                $scope.patientInfo = data;
+                $scope.SmokingHisType = MyFactory.showSmokingStr($scope.patientInfo.SmokingHisType, $scope.patientInfo.SmokingHis);
+            }
+        }, null);
+    }
+
+
+    $scope.approve = function () {
+        alert("发药登记成功！");
+        $("#ApproveDiv").hide();
+    }
+
+    $scope.closeSE = function () {
+        $("#ApproveDiv").hide();
+    }
+
+
+
+
+
 }
+
+
+
